33ee6bdd6bb8559cd46bc6fc45097b6394dfb6e1
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2024-05-05 19:57:21 +0700

    Add project files.

---
+++ UCP1_Pemdas/UCP1_Pemdas.cpp
@@ -0,0 +1,35 @@
+// UCP1_Pemdas.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int nTanggerang, nBekasi;
+int sehat, tidaksehat;
+
+void input() {
+	cout << "masukkan namakota=";
+	cin >> nTanggerang, nBekasi;
+	
+}
+
+void display() {
+	for (75;45;)
+	{
+		cout << "status =";
+		cin >> tidaksehat, sehat;
+
+		while( (75>= tidaksehat)&&(45<= sehat))
+	}
+}
+
+int main() {
+
+}
+
+// 1. nama = string, umur = int, jurusan= string
+// 2. bisa di implentasikan dalam penjualan sembako contoh minyak, beras, dan telur
+// 3. bisa diimplementasikan dalam bilangan 
+// 4. bisa diimplementasi dalam
+// 5. bisa diimplentasikan dalam structur
